1c1bcc8ffcf4db1a4086ecb4e205d9f0a39cff2a
Salwa <salwa.fathallah@hotmail.fr>
2020-04-22 17:09:52 +0100

    add logs

--- tools/spelling.sh
+++ tools/spelling.sh
@@ -3,5 +3,10 @@
 sh ./install-misspell.sh &> /dev/null
 wait
 #mv ./bin/misspell /usr/local/bin
-git ls-files | xargs ./bin/misspell
+FILES= git ls-files | xargs ./bin/misspell
 rm -rf ./bin
+if ${FILES}; then
+  echo "you have misspelled files above, please tun locally the script in 'tools/spelling correct'"
+  exit 1
+fi
+
